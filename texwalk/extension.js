dfb29f51e4fa13e5bde85a4c351b91b8e46671b0
I551990 <constantin.heeren@sap.com>
2023-08-29 11:38:54 +0200

    for typst

--- texwalk/extension.js
+++ texwalk/extension.js
@@ -5,15 +5,19 @@ const vscode = require('vscode');
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
+	vscode.window.showInformationMessage('Activating TexWalk...');
+
 	let disposable = vscode.commands.registerCommand('texwalk.umlautCorection', function () {
 
 		// get open latex document
 		let editor = vscode.window.activeTextEditor;
 		if (!editor) {
+			vscode.window.showInformationMessage('No tyst document open!');
 			return; // No open text editor
 		}
 		// return if editor is not a latex document
-		if (editor.document.languageId != "latex") {
+		if (editor.document.languageId != "typst") {
+			vscode.window.showInformationMessage('No tyst document open!');
 			return;
 		}
 
@@ -22,7 +26,7 @@ function activate(context) {
 		let text = document.getText();
 
 		// get regex for umlaut in latex format
-		let regex = /"[aouAOUsS]/g;
+		let regex = /\\[aouAOUsS]/g;
 		// get all matches
 		let matches = text.match(regex);
 		// return if no matches
@@ -35,14 +39,14 @@ function activate(context) {
 			let match = matches[i];
 
 			// replace latex umlaut with utf-8 umlaut
-			match = match.replace('"a', 'ä');
-			match = match.replace('"o', 'ö');
-			match = match.replace('"u', 'ü');
-			match = match.replace('"A', 'Ä');
-			match = match.replace('"O', 'Ö');
-			match = match.replace('"U', 'Ü');
-			match = match.replace('"s', 'ß');
-			match = match.replace('"S', 'ẞ');
+			match = match.replace('\\a', 'ä');
+			match = match.replace('\\o', 'ö');
+			match = match.replace('\\u', 'ü');
+			match = match.replace('\\A', 'Ä');
+			match = match.replace('\\O', 'Ö');
+			match = match.replace('\\U', 'Ü');
+			match = match.replace('\\s', 'ß');
+			match = match.replace('\\S', 'ẞ');
 
 			// replace match in text
 			text = text.replace(matches[i], match);
